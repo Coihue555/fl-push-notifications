a6cb5aa315011195b36451121f6b079516cbdd9f
Coihue555 <kevlar555@gmail.com>
2022-03-23 17:40:13 -0300

    envio de notificaciones

--- fl_pushing/lib/main.dart
+++ fl_pushing/lib/main.dart
@@ -1,11 +1,29 @@
+import 'package:fl_pushing/services/push_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_pushing/screens/home_screen.dart';
 import 'package:fl_pushing/screens/message_screen.dart';
 
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationService.initializeApp();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- fl_pushing/lib/services/push_notification_service.dart
+++ fl_pushing/lib/services/push_notification_service.dart
@@ -1,1 +1,56 @@
 //SHA-1: 12:B2:FE:5F:26:89:47:A2:95:B1:17:2C:91:32:2F:79:E7:18:EA:82
+
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService{
+
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+  static StreamController<String> _messageStream = StreamController.broadcast();
+  static Stream<String> get messagesStream => _messageStream.stream;
+
+
+  static Future _backgroundHandler(RemoteMessage message) async {
+    print('backgroundHandler: ${message.messageId}');
+    _messageStream.add(message.notification?.title ?? 'Sin titulo');
+  }
+
+  static Future _onMessagedHandler(RemoteMessage message) async {
+    print('onMessagedHandler: ${message.messageId}');
+    _messageStream.add(message.notification?.title ?? 'Sin titulo');
+  }
+
+  static Future _onOpenMessageHandler(RemoteMessage message) async {
+    print('onOpenMessage: ${message.messageId}');
+    _messageStream.add(message.notification?.title ?? 'Sin titulo');
+  }
+  
+  //TODO video 277 Missing Default Notification Channel metadata in AndroidManifest. Default value will be used.
+  //en las consultas del curso hay soluciones
+
+  
+  static Future initializeApp() async {
+
+    //push notifications
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+    print(token);
+
+    //handlers
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessagedHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onOpenMessageHandler);
+
+    //local notifications
+
+  
+
+  }
+
+  static closeStreams(){
+    _messageStream.close();
+  }
+}
